bbbaa4e42aa5b9e85ab57b0c58f845eb87264d9f
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2023-01-12 18:59:43 -0500

    add viewAllEmployees function

--- index.js
+++ index.js
@@ -53,6 +53,15 @@ function runApp() {
         });
 }
 
+async function viewAllEmployees() {
+    console.log(' ');
+    await connection.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        runApp();
+    });
+};
+
 console.log(`
 ╔═══╗─────╔╗──────────────╔═╗╔═╗
 ║╔══╝─────║║──────────────║║╚╝║║
