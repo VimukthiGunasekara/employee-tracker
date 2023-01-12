1fab6c236e1250afcd6e397cf91aec9ed434023d
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2023-01-12 16:49:39 -0500

    add options and logo

--- index.js
+++ index.js
@@ -10,4 +10,61 @@ const connection = mysql.createConnection({
     database: "employeeDB"
 });
 
+function runApp() {
+    inquirer
+        .prompt({
+            type: "list",
+            message: "What would you like to do?",
+            name: "options",
+            choices: ['View All Employees', 'View Department', 'View Role', 'Add Employee', 'Add Department', 'Add Role', 'Update Role']
+        })
+        .then(function (ans) {
+            console.log(ans);
+
+            if (ans.options === "View All Employees") {
+                viewAllEmployees();
+            }
+            else if (ans.options === "View Department") {
+                viewDepts();
+
+            }
+            else if (ans.options === "View role") {
+                viewrole();
+
+            }
+            else if (ans.options === "Add Employee") {
+                addEmployee();
+
+            }
+            else if (ans.options === "Add Department") {
+                addDept();
+
+            }
+            else if (ans.options === "Add Role") {
+                addRole();
+
+            }
+            else if (ans.options === "Update Role") {
+                updateRole();
+
+            } else {
+                connection.end();
+            }
+        });
+}
+
+console.log(`
+╔═══╗─────╔╗──────────────╔═╗╔═╗
+║╔══╝─────║║──────────────║║╚╝║║
+║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+───────║║──────╔═╝║─────────────────────╔═╝║
+───────╚╝──────╚══╝─────────────────────╚══╝
+──────────────────────────────────────────────────
+         Develop by Vimukthi Gunasekara
+──────────────────────────────────────────────────
+──────────────────────────────────────────────────`);
+
 runApp();
